1114300f43076aeae8c04fed4633947b2c38c481
Tri Nha <lychautrinha@gmail.com>
2018-10-08 13:48:17 +0700

    update

--- src/pages/Room/Phong/CustomCard.js
+++ src/pages/Room/Phong/CustomCard.js
@@ -13,13 +13,14 @@ export default class CustomCard extends PureComponent {
         hoverable
         className={styles.card}
         // actions={[<a>操作一</a>, <a>操作二</a>]}
+        // style={{}}
       >
         <Card.Meta
           avatar={
             //   <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
             // <Icon type="lock" style={{ fontSize: '32px',color: '#E33539' }}/>
             // <Icon type="unlock" style={{ fontSize: '32px',color: '#5BBD2B' }}/>
-            <Icon type="book" style={{ fontSize: '32px',color: '#F9F400' }}/>            
+            <Icon type="book" style={{ fontSize: "32px", color: "#F9F400" }} />
           }
           title={this.props.title}
           // description={this.props.description}
